58c387fc0f8f71fdc73371aa400d12ada8b1fb71
elijah <elijahladdie2004@gmail.com>
2024-06-05 23:36:52 +0200

    configure husky,eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import './styles/App.css'
-import WelcomePage from "./pages/welcomePage";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './styles/App.css';
+import WelcomePage from './pages/welcomePage';
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <WelcomePage />
     }
-  ])
+  ]);
   return (
     <div>
       <RouterProvider router={router} />
     </div>
-  ); 
+  );
 }
 
-export default App
+export default App;
